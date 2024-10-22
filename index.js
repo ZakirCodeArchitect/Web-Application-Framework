91934ed430f8c5f309020e7698ea59034c863dc0
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-10-22 12:18:07 +0500

    Lecture 6

--- index.js
+++ index.js
@@ -1,2 +1,68 @@
-const math = require('./node')
-console.log(math)
+// const math = require('./node')
+// console.log(math)
+
+/* Lecture # 6 */
+
+
+const EventEmitter = require("events")
+
+const myEmitter = new EventEmitter()
+
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+
+const pathname = path.join(__dirname, "text.txt")
+
+function dataControl(req, res) {
+    console.log("Server is running ");
+
+    // Log the request details to text.txt
+    const log = `\n ${new Date().getHours().toString()}\t ${req.url} \t New request received`;
+    fs.appendFile("text.txt", log, (err) => {
+        if (err) {
+            console.error("Failed to write to file");
+        }
+    });
+
+    const myURL = url.parse(req.url);
+    console.log(myURL)
+
+    // Routing logic
+    switch (req.url) {
+        case '/':
+            const home = fs.readFileSync("./home.html", "utf-8")
+            res.end(home); // what we are displaying on the web-browser
+            break;
+
+        case '/dashboard':
+            res.write("Dashboard");
+            break;
+
+        case '/contacts':
+            res.write("Contacts")
+                // const user = myURL.query.name;
+                // const id = myURL.query.id;
+                // console.log(user);
+                // console.log(id)
+                // res.write(`This is the userName ${user}`)
+            myEmitter.on("FileLoaded", () => {
+                console.log("Contact provided")
+            })
+            myEmitter.emit("FileLoaded")
+
+            break;
+
+        default:
+            res.write("Nodejs Server Running");
+            break;
+    }
+
+    res.end();
+}
+
+// Create the server
+http.createServer(dataControl).listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
---
+++ indexa.js
@@ -0,0 +1,52 @@
+const path = require('node:path')
+const os = require('os')
+
+const fileName = __filename;
+const dirName = __dirname;
+const baseName = path.basename(__filename);
+const file = path.parse(__filename); // parse[pathconvert path to format] and 
+
+console.log("File path : ", fileName)
+console.log("File Directory : ", dirName)
+console.log("File Base Name : ", baseName)
+console.log("File Extension : ", file.ext) // getting the extension of the file
+
+console.log("Object to path format : ", path.format(file)) // format [ then format converts that information into path ]
+
+console.log("Join different paths : ", path.join("folder1", "folder2", "home.html")) // .. IS USED to jump the folder.
+
+// resolve method   -- converts sequence of path into absolute path means from the start
+console.log("Absolute Path : ", path.resolve(__dirname, "xyz.txt"))
+
+const localMemory = os.totalmem(); // total memory
+const freeMemory = os.freemem(); // free memory
+
+console.log("Local Memory: ", localMemory)
+console.log("Free Memory : ", freeMemory)
+
+/* Events */
+
+
+console.log("\nEVENTS : ")
+
+const EventEmitter = require("events")
+
+const myEmitter = new EventEmitter()
+
+// First listner
+myEmitter.on("FileLoaded", () => {
+    console.log("File is loaded")
+})
+
+// First listner
+// File loaded is file name
+myEmitter.on("FileLoaded", function() {
+        console.log("Second File is loaded")
+    }) // when that event is occued execute particular function.
+
+// third listner
+myEmitter.on("FileLoaded", () => {
+    console.log("Contact provided")
+})
+
+myEmitter.emit("FileLoaded") // Raising event -- to broadcast
